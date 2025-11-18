6e109b50fa1176baba7824276a8878c448beda76
sebastianp21 <sebastianpalomino0210@gmail.com>
2025-11-17 19:15:58 -0500

    add period test for ssh

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
         <Waybar/>
         <div className="landpage_container">
             <h1 className="landName">PcBuilder</h1>
-            <p>A part picker website for computer components</p>
+            <p>A part picker website for computer components.</p>
         </div>
         <Link to="/build">
             <button >Start Build</button>
